46207c79e0e2a77320f64c5c744f91dff77f8719
Steven <steven_harland@hotmail.co.uk>
2016-05-19 20:02:38 +0100

    Add CommonMark property value converter

---
+++ SH.Site/PropertyValueConverters/MarkdownEditorValueConverter.cs
@@ -0,0 +1,42 @@
+﻿using CommonMark;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web.Templates;
+
+namespace SH.Site.PropertyValueConverters
+{
+    [PropertyValueType(typeof(IHtmlString))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Request)]
+    public class MarkdownEditorValueConverter : PropertyValueConverterBase
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return Constants.PropertyEditors.MarkdownEditorAlias.Equals(propertyType.PropertyEditorAlias);
+        }
+
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            var sourceString = source.ToString();
+            sourceString = TemplateUtilities.ParseInternalLinks(sourceString);
+            sourceString = TemplateUtilities.ResolveUrlsFromTextString(sourceString);
+            return sourceString;
+        }
+
+        public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return new HtmlString(source == null ? string.Empty : CommonMarkConverter.Convert(source.ToString()));
+        }
+
+        public override object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+    }
+}
