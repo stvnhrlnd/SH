02cc3358191673d481b0c6085be27203653a8e30
Steven <steven_harland@hotmail.co.uk>
2016-05-25 21:07:49 +0100

    Simplify Markdown value converter

--- SH.Site/PropertyValueConverters/MarkdownEditorValueConverter.cs
+++ SH.Site/PropertyValueConverters/MarkdownEditorValueConverter.cs
@@ -3,12 +3,10 @@ using System.Web;
 using Umbraco.Core;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Core.PropertyEditors;
-using Umbraco.Web.Templates;
 
 namespace SH.Site.PropertyValueConverters
 {
     [PropertyValueType(typeof(IHtmlString))]
-    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Request)]
     public class MarkdownEditorValueConverter : PropertyValueConverterBase
     {
         public override bool IsConverter(PublishedPropertyType propertyType)
@@ -16,27 +14,14 @@ namespace SH.Site.PropertyValueConverters
             return Constants.PropertyEditors.MarkdownEditorAlias.Equals(propertyType.PropertyEditorAlias);
         }
 
-        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object data, bool preview)
         {
-            if (source == null)
-            {
+            var markdown = data as string;
+            if (string.IsNullOrWhiteSpace(markdown))
                 return null;
-            }
 
-            var sourceString = source.ToString();
-            sourceString = TemplateUtilities.ParseInternalLinks(sourceString);
-            sourceString = TemplateUtilities.ResolveUrlsFromTextString(sourceString);
-            return sourceString;
-        }
-
-        public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
-        {
-            return new HtmlString(source == null ? string.Empty : CommonMarkConverter.Convert(source.ToString()));
-        }
-
-        public override object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
-        {
-            return source;
+            var html = CommonMarkConverter.Convert(markdown);
+            return new HtmlString(html);
         }
     }
 }
