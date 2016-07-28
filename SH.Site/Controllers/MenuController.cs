cdf74f8386e48ff2c12e51877b4d2360a26f17cf
Steven <steven_harland@hotmail.co.uk>
2016-07-28 19:10:07 +0100

    Move menu out of master template to child action

---
+++ SH.Site/Controllers/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using SH.Site.Models;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.PublishedContentModels;
+
+namespace SH.Site.Controllers
+{
+    public class MenuController : SurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult Menu()
+        {
+            var website = CurrentPage.Ancestor<Website>();
+
+            var model = new MenuViewModel(CurrentPage);
+            model.SiteName = website.SiteName;
+            model.MenuItems = website.Children<IMaster>();
+
+            return PartialView("_Menu", model);
+        }
+    }
+}
---
+++ SH.Site/Models/MenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+
+namespace SH.Site.Models
+{
+    public class MenuViewModel : RenderModel
+    {
+        public MenuViewModel(IPublishedContent content) : base(content) { }
+
+        public string SiteName { get; set; }
+
+        public IEnumerable<IPublishedContent> MenuItems { get; set; }
+    }
+}
