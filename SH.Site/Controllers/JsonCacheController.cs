2f1f22271b041360221709b457ac871a63eefd70
Steven <steven_harland@hotmail.co.uk>
2017-02-25 11:56:16 +0000

    Refactor JSON cache implementation
    Hook into published event in order to invalidate cache.
    
    Remove dashboard and update public API so that cache is refreshed on demand.

--- SH.Site/Controllers/JsonCacheController.cs
+++ SH.Site/Controllers/JsonCacheController.cs
@@ -5,52 +5,98 @@ using SH.Site.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
-using System.Web;
+using System.Web.Hosting;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Umbraco.Core.Logging;
 using Umbraco.Web.WebApi;
 
 namespace SH.Site.Controllers
 {
-    public class JsonCacheController : UmbracoAuthorizedApiController
+    [EnableCors("*", "*", "*", "ETag")]
+    public class JsonCacheController : UmbracoApiController
     {
         [HttpGet]
-        public HttpResponseMessage Refresh()
+        public HttpResponseMessage Download()
         {
             try
             {
-                // Serialise Umbraco content tree to JSON
-                var content = Umbraco.TypedContentAtRoot();
-                var contentModels = Mapper.Map<IEnumerable<JsonCacheContentModel>>(content);
-                var json = JsonConvert.SerializeObject(contentModels, new JsonSerializerSettings
-                {
-                    ContractResolver = new CamelCasePropertyNamesContractResolver()
-                });
+                // Ensure JSON cache file exists
+                var physicalPath = HostingEnvironment.MapPath(Constants.JsonCachePath);
+                if (!File.Exists(physicalPath))
+                    Refresh();
 
-                // Write JSON to file
-                var physicalPath = HttpContext.Current.Server.MapPath(Constants.JsonCachePath);
-                File.WriteAllText(physicalPath, json, Encoding.UTF8);
+                // Check if the JSON cache has been updated since the last request from the client
+                var md5Hash = File.ReadAllText(HostingEnvironment.MapPath(Constants.JsonCacheMd5Path));
+                var responseETag = EntityTagHeaderValue.Parse($"\"{md5Hash}\"");
+                var requestETag = Request.Headers.IfNoneMatch.FirstOrDefault();
+                if (requestETag != null && requestETag.Tag == responseETag.Tag)
+                    return Request.CreateResponse(HttpStatusCode.NotModified);
 
-                // Compute and store MD5 hash of JSON file
-                using (var stream = File.OpenRead(physicalPath))
-                {
-                    using (var md5 = MD5.Create())
-                    {
-                        var hash = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
-                        File.WriteAllText(HttpContext.Current.Server.MapPath(Constants.JsonCacheMd5Path), hash);
-                    }
-                }
-
-                return Request.CreateResponse(HttpStatusCode.OK);
+                // Create and return JSON response from file
+                var stream = new FileStream(physicalPath, FileMode.Open);
+                var response = Request.CreateResponse(HttpStatusCode.OK);
+                response.Content = new StreamContent(stream);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response.Headers.ETag = responseETag;
+                return response;
             }
             catch (Exception exception)
             {
-                Logger.Error<JsonCacheController>("Failed to refresh JSON cache.", exception);
-                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, exception);
+                Logger.Error<JsonCacheController>("Could not serve JSON cache.", exception);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        public HttpResponseMessage ValidateMd5(string hash)
+        {
+            try
+            {
+                // Ensure JSON cache MD5 exists
+                var physicalPath = HostingEnvironment.MapPath(Constants.JsonCacheMd5Path);
+                if (!File.Exists(physicalPath))
+                    Refresh();
+
+                // Compare request hash with hash on file
+                var fileHash = File.ReadAllText(physicalPath);
+                return Request.CreateResponse(hash == fileHash);
+            }
+            catch (Exception exception)
+            {
+                Logger.Error<JsonCacheController>("Could not validate JSON cache MD5.", exception);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private void Refresh()
+        {
+            // Serialise Umbraco content tree to JSON
+            var content = Umbraco.TypedContentAtRoot();
+            var contentModels = Mapper.Map<IEnumerable<JsonCacheContentModel>>(content);
+            var json = JsonConvert.SerializeObject(contentModels, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+
+            // Write JSON to file
+            var physicalPath = HostingEnvironment.MapPath(Constants.JsonCachePath);
+            File.WriteAllText(physicalPath, json, Encoding.UTF8);
+
+            // Compute and store MD5 hash of JSON file
+            using (var stream = File.OpenRead(physicalPath))
+            {
+                using (var md5 = MD5.Create())
+                {
+                    var hash = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                    File.WriteAllText(HostingEnvironment.MapPath(Constants.JsonCacheMd5Path), hash);
+                }
             }
         }
     }
--- SH.Site/EventHandlers/JsonCacheEventHandler.cs
+++ SH.Site/EventHandlers/JsonCacheEventHandler.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using SH.Site.Models;
 using System.Linq;
+using System.Web.Hosting;
 using Umbraco.Core;
+using Umbraco.Core.Events;
 using Umbraco.Core.Models;
+using Umbraco.Core.Publishing;
+using Umbraco.Core.Services;
 using Umbraco.Web;
 
 namespace SH.Site.EventHandlers
@@ -12,10 +16,21 @@ namespace SH.Site.EventHandlers
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication,
             ApplicationContext applicationContext)
         {
+            ContentService.Published += ContentService_Published;
+
             Mapper.CreateMap<IPublishedContent, JsonCacheContentModel>()
                 .ForMember(m => m.Content,
                     o => o.ResolveUsing(c => c.Properties.ToDictionary(p => p.PropertyTypeAlias, p => p.DataValue)))
                 .ForMember(m => m.Template, o => o.ResolveUsing(c => c.GetTemplateAlias()));
+        }
+
+        private void ContentService_Published(IPublishingStrategy sender, PublishEventArgs<IContent> e)
+        {
+            // Invalidate JSON cache
+            var jsonCachePath = HostingEnvironment.MapPath(Constants.JsonCachePath);
+            var jsonCacheMd5Path = HostingEnvironment.MapPath(Constants.JsonCacheMd5Path);
+            System.IO.File.Delete(jsonCachePath);
+            System.IO.File.Delete(jsonCacheMd5Path);
         }
     }
 }
